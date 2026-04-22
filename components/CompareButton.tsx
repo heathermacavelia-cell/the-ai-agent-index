126d7a66df6902637983b0e455ce7ed84dec6ff5
heathermacavelia-cell <heather.macavelia@gmail.com>
2026-04-22 11:45:53 -0400

    Remove duplicate plus on compare button

--- components/CompareButton.tsx
+++ components/CompareButton.tsx
@@ -43,7 +43,7 @@ export default function CompareButton({ agent }: CompareButtonProps) {
               <line x1="12" y1="5" x2="12" y2="19" />
               <line x1="5" y1="12" x2="19" y2="12" />
             </svg>
-            + Compare Board
+            Compare Board
           </>
         )}
       </button>
